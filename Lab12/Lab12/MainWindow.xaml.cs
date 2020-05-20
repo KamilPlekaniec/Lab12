4e29d0f79ba79ef63a54434498615e771f60d4cd
KamilPlekaniec <kp054467@student.ath.edu.pl>
2020-05-20 15:09:27 +0200

    New files

---
+++ Lab12/Lab12/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Lab12
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<User> users = new List<User>()
+        {
+            new User("maniek12", "mleko123"),
+            new User("manio", "123"),
+            new User("man", "mleko"),
+            new User("12", "m3")
+            
+        };
+        public bool CheckLogin(string login, SecureString password)
+        {
+            var pwd = default(string);
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(password);
+                pwd = Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+
+            return (Login == login && Password == pwd);
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void WylogujBTN_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ZalogujBTN_Click(object sender, RoutedEventArgs e)
+        {
+            ZalogujBTN.Visibility = Visibility.Hidden;
+            WylogujBTN.Visibility = Visibility.Visible;
+        }
+
+        private void LoginTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Lab12/Lab12/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    public class User
+    {
+        public User(string login, string password)
+        {
+            Password = password;
+            Login = login;
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        
+        
+    }
+}
